96dc5f7edc70ad8483f58d4444c0c1f6d6da46cf
holic512 <2423580694@qq.com>
2024-05-26 23:21:53 +0800

    1.补充提现表格类型

--- frontend/src/views/AdminPage/UserMm/WithdrawMm/index.vue
+++ frontend/src/views/AdminPage/UserMm/WithdrawMm/index.vue
@@ -19,13 +19,18 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
 
   <topNavText :text="'用户管理-提现管理'"/>
   <el-card>
+   选择栏 提现id 商户名称 商店名称 提现方式 备注  处理 （完成 拒绝） 批量处理 （批量接受 批量拒绝）
     <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
       <el-tab-pane label="未处理" name="first">
 
         <el-table :data="tableData" stripe style="width: 100%">
-          <el-table-column prop="date" label="Date" width="180" />
+
+          <el-table-column prop="id" label="Date" width="180" />
+
           <el-table-column prop="name" label="Name" width="180" />
+
           <el-table-column prop="address" label="Address" />
+
         </el-table>
 
       </el-tab-pane>
